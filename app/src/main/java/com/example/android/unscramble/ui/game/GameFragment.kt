ff2f1516f40aedb407f72ef96ee6f5318af5d4c5
Dimaslv2 <muhammaddimaslv2@gmail.com>
2023-09-24 13:33:38 +0700

    6-9

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -87,6 +87,16 @@ class GameFragment : Fragment() {
     /*
  * Creates and shows an AlertDialog with the final score.
  */
+
+    /*
+     * Gets a random word for the list of words and shuffles the letters in it.
+     */
+    private fun getNextScrambledWord(): String {
+        val tempWord = allWordsList.random().toCharArray()
+        tempWord.shuffle()
+        return String(tempWord)
+    }
+
     private fun showFinalScoreDialog() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
@@ -100,17 +110,6 @@ class GameFragment : Fragment() {
             }
             .show()
     }
-
-    /*
-     * Gets a random word for the list of words and shuffles the letters in it.
-     */
-    private fun getNextScrambledWord(): String {
-        val tempWord = allWordsList.random().toCharArray()
-        tempWord.shuffle()
-        return String(tempWord)
-    }
-
-
 
     /*
      * Re-initializes the data in the ViewModel and updates the views with the new data, to
